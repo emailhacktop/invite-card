11e2b5182d6e418020af408e999e64771e61f433
emailhacktop <emailhacktop@gmail.com>
2026-06-26 19:32:21 +0330

    Add files via upload

--- app.js
+++ app.js
@@ -68,16 +68,8 @@ if (Number.isNaN(currentViews) || currentViews >= maxViews) {
   if (expiredTitle) {
     expiredTitle.innerHTML = `
       لینک منقضی شده است
-      <div style="
-      font-size:4px !important;
-      line-height:4px !important;
-      opacity:.18;
-      margin-top:2px;
-      transform:scale(.45);
-      transform-origin:center;
-      letter-spacing:0;
-      ">
-        برای ریست روی لوگو بزنید
+      <div style="font-size:5px;opacity:.25;margin-top:8px;">
+         ریست کنید
       </div>
     `;
   }
